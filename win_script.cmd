1b17089fee306805ecd111e52b4d38e453260fb7
RebelSlicer <54483464+RebelSlicer@users.noreply.github.com>
2021-02-20 16:06:22 -0500

    Update win_script.cmd

--- win_script.cmd
+++ win_script.cmd
@@ -238,7 +238,7 @@ netsh advfirewall firewall add rule name="Block SyncAppvPublishingServer.exe net
 netsh advfirewall firewall add rule name="Block wmic.exe netconns" program="%systemroot%\SysWOW64\wbem\wmic.exe" protocol=tcp dir=out enable=yes action=block profile=any
 netsh advfirewall firewall add rule name="Block wscript.exe netconns" program="%systemroot%\SysWOW64\wscript.exe" protocol=tcp dir=out enable=yes action=block profile=any
 netsh advfirewall firewall set rule group="windows management instrumentation (wmi)" new enable=no
-for /R %f in (powershell*.exe) do (netsh advfirewall firewall add rule name=“PS-Deny-All (%f)" dir=out action=block program=“%f" enable=yes)
+netsh advfirewall firewall add rule name=“PS-Deny-All" dir=out action=block program="c:\windows\system32\WindowsPowerShell\v1.0\powershell.exe" enable=yes
 
 ::Disable WinRM
 net stop WinRM
